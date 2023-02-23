2fe0e2e481f65fbdb9fbf632747d59e0bab41aaf
harshraoka <81157043+harshraoka@users.noreply.github.com>
2023-02-23 19:00:28 +0530

    Feat: Added Symptom data and tables to prediction page

--- client/src/Pages/DiseasePrediction.jsx
+++ client/src/Pages/DiseasePrediction.jsx
@@ -1,13 +1,111 @@
-import React from 'react'
+import symptoms from "../data/Symptoms";
 
 
+function GetData() {
+    var checkboxes = document.getElementsByID("check");
+    var result = [];
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result.push(1);
+            // console.log("1")
+        } else {
+            result.push(0);
+            // console.log("0")
+        }
+    }
+    let person = {
+        symptoms: [result],
+    };
+    console.log(person);
+
+    fetch("http://127.0.0.1:2000/", {
+        method: "POST",
+        body: JSON.stringify({
+            symptoms: result,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+        .then((response) => {
+            if (response.status !== 200) {
+                console.log(
+                    "Looks like there was a problem. Status Code: " + response.status
+                );
+                return;
+            }
+
+            console.log(response.headers.get("Content-Type"));
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(JSON.stringify(myJson));
+            alert("You might have :" + myJson.disease);
+        })
+        .catch((err) => {
+            console.log("Fetch Error :-S", err);
+        });
+}
+
+function GetCheck() {
+    var checkboxes = document.getElementsByTagName("input");
+    var result = [];
+
+    for (var i = 1; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result.push(1);
+            // console.log("1")
+        } else {
+            result.push(0);
+            // console.log("0")
+        }
+    }
+    let person = {
+        symptoms: [result],
+    };
+    console.log(person);
+}
 
 
 const DiseasePrediction = () => {
     return (
         <>
-            <div>
-               <h1>hello world</h1>
+            <div className="content-center">
+                <div className="App">
+                    <h1>Please select any Symptoms you might have:</h1>
+                    <div >
+                        <h1 className="text-centre">Symptoms</h1>
+                        <table >
+                            <thead>
+                                <tr className="">
+                                    <th ></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {symptoms.map((total) => {
+                                    return (
+                                        <tr className="even:bg-slate-200 odd:bg-gray-400 text-black hover:bg-green-600">
+                                            <td className="m-2"><input id="check" type="checkbox" className="m-2 w-7 h-7 rounded focus:bg-black" /></td>
+                                            <td className="w-full"><div className="" id={total.id}>{total.str}</div></td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                        <button id="getData" className="bg-indigo-500  p-5 rounded-xl mt-5 " onClick={GetCheck}>
+                            GET DIAGNOSIS
+                        </button>
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
             </div>
         </>
     )
---
+++ client/src/data/Symptoms.js
@@ -0,0 +1,10 @@
+const symptoms = [
+    { id: 1, str: "taking injections" },
+    { id: 2, str: "cough" },
+    { id: 3, str: "pain in chest" },
+    { id: 4, str: "pus in pimples" },
+    { id: 5, str: "pain in abdomen area" },
+    { id: 6, str: "pain in one body side i get old dsfaldkf dsfolkasdfnlo dasikjflkasdf diofpja" },
+  ];
+  
+  export default symptoms;
