28d3a471a6d938d926bbc776338b0bbbd00a7df0
harshraoka <81157043+harshraoka@users.noreply.github.com>
2023-02-25 12:18:48 +0530

    Feat: Added Modal To AI Predction

--- client/src/Pages/DiseasePrediction.jsx
+++ client/src/Pages/DiseasePrediction.jsx
@@ -1,85 +1,78 @@
 import symptoms from "../data/Symptoms";
-
-
-function GetData() {
-    var checkboxes = document.getElementsByTagName("input");
-    var result = [];
-
-    for (var i = 1; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            result.push(1);
-            // console.log("1")
-        } else {
-            result.push(0);
-            // console.log("0")
-        }
-    }
-    let person = {
-        symptoms: [result],
-    };
-    console.log(person);
-
-    fetch("http://127.0.0.1:2000/", {
-        method: "POST",
-        body: JSON.stringify({
-            symptoms: result,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    })
-        .then((response) => {
-            if (response.status !== 200) {
-                console.log(
-                    "Looks like there was a problem. Status Code: " + response.status
-                );
-                return;
-            }
-
-            console.log(response.headers.get("Content-Type"));
-            return response.json();
-        })
-        .then((myJson) => {
-            console.log(JSON.stringify(myJson));
-            alert("You might have :" + myJson.disease);
-        })
-        .catch((err) => {
-            console.log("Fetch Error :-S", err);
-        });
-}
-
-function GetCheck() {
-    var checkboxes = document.getElementsByTagName("input");
-    var result = [];
-
-    for (var i = 1; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            result.push(1);
-            // console.log("1")
-        } else {
-            result.push(0);
-            // console.log("0")
-        }
-    }
-    let person = {
-        symptoms: [result],
-    };
-    console.log(person);
-}
-
+import Modal from 'react-modal'
+import { useState } from "react";
+Modal.setAppElement('#root');
+var diagnosis = "hello";
 
 const DiseasePrediction = () => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    function GetData() {
+        var checkboxes = document.getElementsByTagName("input");
+        var result = [];
+
+        for (var i = 1; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                result.push(1);
+                // console.log("1")
+            } else {
+                result.push(0);
+                // console.log("0")
+            }
+        }
+        let person = {
+            symptoms: [result],
+        };
+        console.log(person);
+
+        fetch("http://127.0.0.1:2000/", {
+            method: "POST",
+            body: JSON.stringify({
+                symptoms: result,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => {
+                if (response.status !== 200) {
+                    console.log(
+                        "Looks like there was a problem. Status Code: " + response.status
+                    );
+                    return;
+                }
+
+                console.log(response.headers.get("Content-Type"));
+                return response.json();
+            })
+            .then((myJson) => {
+                console.log(JSON.stringify(myJson));
+                // alert("You might have :" + myJson.disease);
+                diagnosis = myJson.disease;
+                setModalIsOpen(true)
+            })
+            .catch((err) => {
+                console.log("Fetch Error :-S", err);
+            });
+
+    }
     return (
         <>
+
+
+           
+        
+    
+          
             <div className="m-5 p-5 text-2xl">
-            <h1>Please select any Symptoms you might have:</h1>
+                <h1>Please select any Symptoms you might have:</h1>
             </div>
-            
+
             <div className="p-5 m-5">
                 <div className="">
-                    
+
                     <div >
-                        
+
                         <table className="" >
                             <thead>
                                 <tr className="">
@@ -98,11 +91,11 @@ const DiseasePrediction = () => {
                             </tbody>
                         </table>
                         <div className="w-full flex justify-center ">
-                        <button id="getData" className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mt-5 p-5 rounded-2xl" onClick={GetData}>
-                            GET DIAGNOSIS
-                        </button>
+                            <button id="getData" className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mt-5 p-5 rounded-2xl" onClick={GetData}>
+                                GET DIAGNOSIS
+                            </button>
                         </div>
-                      
+
                     </div>
 
 
@@ -113,6 +106,14 @@ const DiseasePrediction = () => {
 
 
                 </div>
+                <div className="flex flex-row  justify-center items-center"> 
+            <Modal className="justify-center items-center bg-slate-100 w-fit h-fit mt-20 mr-auto ml-auto border-solid border-2 border-black pl-20 pr-20 pt-5 pb-5  rounded-2xl" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                <div> <h1 className="text-bold">You may have</h1></div>
+                <div><h2 className="text-2xl font-bold">{diagnosis}</h2></div>
+                <div><button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mt-5 p-5 rounded-2xl" onClick={() => setModalIsOpen(false)}>Close</button></div>
+                
+            </Modal>
+            </div>
             </div>
         </>
     )
