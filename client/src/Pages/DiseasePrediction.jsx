f43eae527c204c0c4364d7653c9e8f8a2cb1c1c0
harshraoka <81157043+harshraoka@users.noreply.github.com>
2023-02-25 14:17:23 +0530

    Feat: Added a loading screen and minor style changes

--- client/src/Pages/DiseasePrediction.jsx
+++ client/src/Pages/DiseasePrediction.jsx
@@ -7,7 +7,9 @@ var diagnosis = "hello";
 const DiseasePrediction = () => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [isloading, setIsloading] = useState(false)
     function GetData() {
+        setIsloading(true)
         var checkboxes = document.getElementsByTagName("input");
         var result = [];
 
@@ -49,6 +51,7 @@ const DiseasePrediction = () => {
                 console.log(JSON.stringify(myJson));
                 // alert("You might have :" + myJson.disease);
                 diagnosis = myJson.disease;
+                setIsloading(false)
                 setModalIsOpen(true)
             })
             .catch((err) => {
@@ -60,10 +63,10 @@ const DiseasePrediction = () => {
         <>
 
 
-           
-        
-    
-          
+
+
+
+
             <div className="m-5 p-5 text-2xl">
                 <h1>Please select any Symptoms you might have:</h1>
             </div>
@@ -106,14 +109,27 @@ const DiseasePrediction = () => {
 
 
                 </div>
-                <div className="flex flex-row  justify-center items-center"> 
-            <Modal className="justify-center items-center bg-slate-100 w-fit h-fit mt-20 mr-auto ml-auto border-solid border-2 border-black pl-20 pr-20 pt-5 pb-5  rounded-2xl" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <div> <h1 className="text-bold">You may have</h1></div>
-                <div><h2 className="text-2xl font-bold">{diagnosis}</h2></div>
-                <div><button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mt-5 p-5 rounded-2xl" onClick={() => setModalIsOpen(false)}>Close</button></div>
+                <div className="flex flex-row  justify-center items-center">
+                    <Modal className="justify-center items-center bg-slate-100 w-fit h-fit mt-20 mr-auto ml-auto border-solid border-2 border-black pl-20 pr-20 pt-5 pb-5  rounded-2xl" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                        <div> <h1 className="font-bold">You may have</h1></div>
+                        <div><h2 className="text-2xl font-bold">{diagnosis}</h2></div>
+                        <div><p className="text-red-500 font-bold">NOTE :This is predicted using an AI</p></div>
+                        <div><p className="text-red-500 font-bold">Please contact a health professional</p></div>
+                        <div><button className="bg-violet-500 text-white hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 mt-5 p-5 rounded-2xl" onClick={() => setModalIsOpen(false)}>Close</button></div>
+
+                    </Modal>
+                    <div className="flex flex-row  justify-center items-center">
+                    <Modal className="justify-center items-center mt-20 mr-auto ml-auto h-fit w-fit bg-slate" isOpen={isloading} onRequestClose={() => isloading(false)}>
+                    <div class="">
+                        <div class="flex justify-center items-center w-20 h-20 overflow-hidden">
+                            <img class="h-20 w-20" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt=""></img>
+                        </div>
+                    </div>
+                    </Modal>
+                    </div>
+                    
                 
-            </Modal>
-            </div>
+                </div>
             </div>
         </>
     )
