ec7de4d787ab5c75eb8274fe426861a4188ce641
vipuldud <126098067+vipuldud@users.noreply.github.com>
2023-03-09 15:33:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import HomeLayout from './Layout/Home.Layout';
 // pages
 import Home from './Pages/Home';
 import DiseasePrediction from './Pages/DiseasePrediction';
+import OrganDonation from './Pages/OrganDonation';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route path='/' element={<HomeLayout />}>
           <Route path='/' element={<Home />}/>
           <Route path='/DiseasePrediction' element={<DiseasePrediction />}/>
+           <Route path='/OrganDonation' element={<OrganDonation/>}/> 
         </Route>
       </Routes>
     </>
