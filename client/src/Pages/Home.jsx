f749157c310cf0c0b5ede7ed7cc903e42b2a4e5d
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-05-13 21:53:12 +0530

    appointment cards mapped

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,18 +4,31 @@ import React from 'react'
 import AppointmentCard from '../components/Home/AppointmentCard'
 
 const Home = () => {
+    const data = [
+        {
+            name: "Anurag Krishna Yadav",
+            post: "Physician",
+            date: "Monday, 02/01/2023",
+            address: "Katra, Prayagraj, UP",
+            time: "10:20-10:20",
+            isActive: 1,
+        },
+        {
+            name: "Mahesh",
+            post: "Physician",
+            date: "Monday, 02/01/2023",
+            address: "Katra, Prayagraj, UP",
+            time: "10:20-10:20",
+            isActive: 0,
+        },
+    ]
+
     return (
         <>
             <div className='flex flex-wrap gap-3'>
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
-                <AppointmentCard />
+                {data.map((tab) => (
+                    <AppointmentCard {...tab} key={`123${tab.route}`} />
+                ))}
             </div>
         </>
     )
